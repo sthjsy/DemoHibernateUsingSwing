b865b7f74907c82ec730917b13addbf99a2d069b
Jasveer <31476758+sthjsy@users.noreply.github.com>
2019-04-14 18:34:19 +0530

    Add files via upload

---
+++ Emp.java
@@ -0,0 +1,27 @@
+package mypack;
+
+public class Emp {
+	private String eno;
+	private String ename;
+	private String esal;
+	public String getEno() {
+		return eno;
+	}
+	public void setEno(String eno) {
+		this.eno = eno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getEsal() {
+		return esal;
+	}
+	public void setEsal(String esal) {
+		this.esal = esal;
+	}
+
+	
+}
